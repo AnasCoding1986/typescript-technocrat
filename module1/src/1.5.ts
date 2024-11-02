601635e074d24d1de72f6f47ba4584c8ac708b2a
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-11-03 04:22:17 +0600

    Object, Optional, Literal Type

---
+++ module1/src/1.5.ts
@@ -0,0 +1,12 @@
+const user:{
+    company: "Programming Hero"; // type-->literal types
+    firstname: string;
+    middlename?: string; // optional type
+    lastname: string;
+    isMarried: boolean
+} = {
+    company: "Programming Hero",
+    firstname: "Azizul Chowdhury",
+    lastname: "Chowdhury",
+    isMarried: true
+}
