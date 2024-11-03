af701c1ba06e55e85b0e002237e121f7a0594087
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-11-03 19:35:20 +0600

    Ternari, Optional Chaining & Nullish Coalescing 2

--- module1/src/1.11.ts
+++ module1/src/1.11.ts
@@ -1,4 +1,31 @@
 {
     // Ternari, Optional Chaining & Nullish Coalescing
+    const age:number = 15;
+    const isAdult = age>=18 ? "adult" : "not adult";
+    console.log({isAdult});
+
+    // Optional Chaining
+    type User = {
+        name: string,
+        address: {
+            city: string,
+            road: string,
+            presentAdd: string,
+            permanentAdd?: string
+        }
+    }
+
+    const user: User = {
+        name: "ki",
+        address: {
+            road: "Sylhet",
+            city: "Sodor",
+            presentAdd: "loi"
+        }
+    }
+
+    const permanentAdd = user?.address?.permanentAdd ?? "No permanent addd";
+    console.log({permanentAdd});
+    
 
 }
