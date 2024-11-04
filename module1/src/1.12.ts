75e453fb49ef40e84003c9203a34a491378e940d
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-11-04 19:53:15 +0600

    Never, Unknown, Nullable completed

--- module1/src/1.12.ts
+++ module1/src/1.12.ts
@@ -1,1 +1,38 @@
-//  Never, Unknown, Nullable
+{
+    //  Never, Unknown, Nullable
+    // Nullable types
+
+    const searchFunc= (value:string) => {
+        if (value) {
+            console.log("Searching");
+        } else {
+            console.log("nothing to search");
+            
+        }
+    };
+
+    searchFunc("");
+
+
+    // Unknown type of
+    const getSpeedInMeterPerSeceond = (value:unknown) => {
+        if (typeof value === "number") {
+            const convertedSpeed = (value*1000)/3600;
+            console.log(`Converted Speed ${convertedSpeed}`);
+        } if (typeof value === "string") {
+            const [res,unit] = value.split(" ");
+            const convertedSpeed = (parseInt(res)*1000)/3600;
+            console.log(`Converted Speed ${convertedSpeed}`);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
