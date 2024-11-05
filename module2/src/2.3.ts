4c09f47528fed98a5acb40e0912647c0bd2426cc
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-11-05 22:04:20 +0600

    Generic

---
+++ module2/src/2.3.ts
@@ -0,0 +1,15 @@
+{
+//
+type ArrayBool = Array<boolean>;
+
+const rollNum : Array<number> = [1,2,3];
+const rollStr : string[] = ["cd","ht","lo"];
+const rollBool : ArrayBool = [true,false,false];
+
+
+
+
+
+
+//
+}
