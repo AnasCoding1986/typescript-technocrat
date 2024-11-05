21d6af8147846acbf241b2e5d077b3b959998e62
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-11-05 21:17:03 +0600

    interface in array

--- module2/src/1.2.ts
+++ module2/src/1.2.ts
@@ -2,7 +2,34 @@
 //
 // Interface , type vs interface
 
+type user1 = {
+    name: string,
+    id: number
+} 
+interface user2 {
+    name: string,
+    id: number
+}
 
+const person : user2 = {
+    name: "li",
+    id: 12
+}
+
+// intesection
+
+type userWithRole1 = user1 & {role:string};
+interface userWithRole2 extends user2 {role:string};
+
+
+// interface in array
+type rool1 = number[];
+
+interface rool2 {
+    [index: number] : number
+}
+
+const one : rool1 = [1,2,3]
 
 
 
