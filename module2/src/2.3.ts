4b78a5048c7ceb05c427ddbfd60408d8dfb8208d
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-11-06 07:05:02 +0600

    declare common generic array

--- module2/src/2.3.ts
+++ module2/src/2.3.ts
@@ -1,9 +1,11 @@
 {
 //
 type ArrayBool = Array<boolean>;
+// declare generic array
+type genericArray<param> = Array<param>;
 
-const rollNum : Array<number> = [1,2,3];
-const rollStr : string[] = ["cd","ht","lo"];
+const rollNum : genericArray<number> = [1,2,3];
+const rollStr : genericArray<string> = ["cd","ht","lo"];
 const rollBool : ArrayBool = [true,false,false];
 
 
