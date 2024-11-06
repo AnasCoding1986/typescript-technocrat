de1ee471acd300e0e93b13887b9cbbe55dc9f4c3
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-11-06 07:55:27 +0600

    geenric tuple

--- module2/src/2.3.ts
+++ module2/src/2.3.ts
@@ -2,12 +2,34 @@
 //
 type ArrayBool = Array<boolean>;
 // declare generic array
-type genericArray<param> = Array<param>;
+type genericArray<T> = Array<T>;
 
 const rollNum : genericArray<number> = [1,2,3];
 const rollStr : genericArray<string> = ["cd","ht","lo"];
 const rollBool : ArrayBool = [true,false,false];
 
+// geenric object
+
+const user : genericArray<{name:string,id:number}> = [
+    {name:"lf",id:23},
+    {name:"hf",id:29},
+]
+
+// geenric tuple
+
+type genericTuple<x,y> = [x,y];
+
+const user3 : genericTuple<string,number>= [
+    'ns', 78
+]
+
+
+const user4 : genericTuple<string,{name:string,pohne:number}>= [
+    'ns', {
+        name: "kdjd",
+        pohne: 876768
+    }
+]
 
 
 
