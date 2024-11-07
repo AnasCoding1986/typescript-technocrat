d47d9a8903017b1608d9bf95ba452267ac0b92f1
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-11-07 06:46:37 +0600

    Function with generic completed

--- module2/src/2.5.ts
+++ module2/src/2.5.ts
@@ -1,3 +1,26 @@
 // Function with generic
 
-const createArray
+const createArray = (param : string) : string[] => {
+    return [param]
+}
+
+const createArrayWithGeneric = <T>(param:T):T[]=>{
+    return [param]
+}
+type UCser = {
+    id:number;
+    name:string
+}
+const user1 = createArrayWithGeneric<UCser>({id:1,name:"kofke"})
+
+createArrayWithGeneric<string>("Bangladesh")
+createArrayWithGeneric<boolean>(true)
+
+// const createArrayWithTuple = <T,Q>(num1:T, num2:Q):[T:Q] => {
+//     return [num1,num2]
+// }
+
+const createArrayWithTuple = <T, Q>(num1: T, num2: Q): [T, Q] => {
+    return [num1, num2];
+}
+
