6f21e2fca7d6823a71c9e1618b82d780a3324bca
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-11-07 08:59:34 +0600

    Constraint using key of not finished

--- module2/src/2.7.ts
+++ module2/src/2.7.ts
@@ -1,6 +1,17 @@
 {
     //
     // Constraint using key of
+    type Vehicle = {
+        bike: string,
+        car: string,
+        ship: string
+    }
+
+    type res1 = "bike" | "car" | "ship";
+
+    type res2 = keyof Vehicle;
+
+    // Constraint using key of not finished
 
 
 
