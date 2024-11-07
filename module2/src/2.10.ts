805a07e01d6ea4b839339a502f4a9e6d95c79ee5
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-11-07 22:19:20 +0600

    Mapped types not finished

--- module2/src/2.10.ts
+++ module2/src/2.10.ts
@@ -1,9 +1,28 @@
 {
     //
     // Mapped types
+    const matArray:number []= [1,2,3,4];
+    
+    const staringArray : string[]= matArray.map(num=>num.toString());
+    console.log(staringArray);
 
+    type arrayNumber = {
+        height: number;
+        width: number
+    }
 
+    // type arrayString = {
+    //     height: string;
+    //     width: string
+    // }
 
+    type arrayString = {
+        [key in keyof arrayNumber] : string
+    }
+    
+    
+
+// Mapped types not finished
 
 
 
