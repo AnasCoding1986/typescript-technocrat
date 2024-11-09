e67a11c393133d3fd94f9031b0b8a25af7f2e0f1
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-11-09 06:40:22 +0600

    module 3 OOP start

---
+++ module3/src/3.1.ts
@@ -0,0 +1,30 @@
+{
+    //
+    // Class and object
+
+    class Animal{
+        name: string;
+        specis: string;
+        sound: string;
+
+        constructor( name: string, specis: string, sound: string){
+            this.name= name;
+            this.specis= specis;
+            this.sound= sound;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //
+}
