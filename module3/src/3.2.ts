a5f53b67fea1d1f3fb4de7db7229b5b42fb35fbe
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-11-09 12:34:31 +0600

    heritance completed

--- module3/src/3.2.ts
+++ module3/src/3.2.ts
@@ -3,6 +3,43 @@
     // Inheritance in OOP
 
 
+    class Parent {
+        name:string;
+        age:number;
+        address:string;
+        constructor(name:string,age:number,address:string){
+            this.name=name,
+            this.age=age,
+            this.address=address
+        }
+        sleeoHour(hour:number):string{
+            return `${this.name} sleep daily ${hour}`
+        }
+    }
+
+    class Student extends Parent{
+            constructor( name:string,age:number,address:string){
+                super(name,age,address)
+            }
+
+    }
+
+    const student1 = new Student("sara", 5, "Sylhet")
+
+    class Teacher extends Parent {
+        designation:string;
+        constructor( name:string, age:number, address:string, designation:string){
+            super(name,age,address)
+            this.designation=designation
+        }
+        takeClass(hour:number):string{
+            return `${this.name} take ${hour} hour class daily`
+        }
+    }
+
+    const teacher1 = new Teacher("sara", 5, "Sylhet", "professor")
+
+
 
 
 
